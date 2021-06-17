681e6fc1ae98d27a4f56d5632c1dfa8b5fa3e0fc
ahajnal <ahajnal@sztaki.hu>
2021-06-17 11:31:01 +0200

    Update build.sh

--- docker-compose/build.sh
+++ docker-compose/build.sh
@@ -2,6 +2,6 @@
 docker-compose -f ./single-node-kafka-docker-compose.yml build
 docker-compose -f ./single-node-kafka-docker-compose.yml up -d
 # docker-compose -f ./single-node-kafka-docker-compose.yml down
-                     s
+                     
 # docker-compose -f ./multi-node-kafka-docker-compose.yml up -d
 # docker-compose -f ./multi-node-kafka-docker-compose.yml down
